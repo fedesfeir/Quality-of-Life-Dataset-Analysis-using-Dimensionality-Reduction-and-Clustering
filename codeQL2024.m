6a96ff17a82a9238974beb0dc395e612c1cf9ad7
Federica Sfeir <sfeirfederica@gmail.com>
2025-09-27 19:19:29 +0200

    Update codeQL2024.m

--- codeQL2024.m
+++ codeQL2024.m
@@ -3,19 +3,24 @@ rng(09042002)
 rowp = randperm(178);
 Xp=X(rowp, :);
 Xr=Xp(1:60,:);
+
 %standarize data
 n=size(Xr,1)
 J=size(Xr,2)
 Jc=eye(n,n)-(1/n)*ones(n,n)
+
 %coveriance matrix
 Sc=(1/60)*Xr'*Jc*Xr;
 D=diag(diag(Sc).^0.5);
 Z=Jc*Xr*D^(-1);
+
 %correlation matrix
 Sz=(1/60)*Z'*Z;
+
 %2. Cdistv=pdist(Z,"euclidean");
 distv=pdist(Z,"euclidean");
 Dist= squareform(distv);
+
 %3. Compute the WSPP;
 [UOtt2,bOtt2, aOtt2, fOtt2,iterOtt2]=WSPP(Dist, 2, 50);
 [pf2,Dw2,Db2] = psF(Z,UOtt2)
@@ -26,6 +31,7 @@ sum(UOtt3)
 [UOtt4,bOtt4, aOtt4, fOtt4,iterOtt4]=WSPP(Dist, 4, 50);
 [pf4,Dw4,Db4] = psF(Z,UOtt4)
 sum(UOtt4)
+
 %4. Compute the WSP;
 [UOtt2,DbOtt2, DwOtt2, fOtt2,iterOtt2]=WSP(Dist, 2, 50);
 [pf2,Dw2,Db2] = psF(Z,UOtt2)
@@ -33,6 +39,7 @@ sum(UOtt4)
 [pf3,Dw3,Db3] = psF(Z,UOtt3)
 [UOtt4,DbOtt4, DwOtt4, fOtt4,iterOtt4]=WSP(Dist, 4, 50);
 [pf4,Dw4,Db4] = psF(Z,UOtt4)
+
 %5. Compute the PD;
 [UOtt2,DbOtt2, DwOtt2, fOtt2,iterOtt2]=PD(Dist, 2, 50);
 [UOtt3,DbOtt3, DwOtt3, fOtt3,iterOtt3]=PD(Dist, 3, 50);
@@ -43,6 +50,7 @@ sum(UOtt4)
 Q2 = UOtt2*DbOtt2*UOtt2'+UOtt2*DwOtt2*UOtt2'-diag(diag(UOtt2*DwOtt2*UOtt2'));
 Q3 = UOtt3*DbOtt3*UOtt3'+UOtt3*DwOtt3*UOtt3'-diag(diag(UOtt3*DwOtt3*UOtt3'));
 Q4 = UOtt4*DbOtt4*UOtt4'+UOtt4*DwOtt4*UOtt4'-diag(diag(UOtt4*DwOtt4*UOtt4'));
+
 %verify ultramtric
 verultrametrica(Q2)
 verultrametrica(Q3)
@@ -50,14 +58,17 @@ verultrametrica(Q4)
 Z3 = linkage(Q3);
 figure();
 dendrogram(Z3,0)
+
 %2. Compute PCA to determine the number of Principal Components;
 [A,L]=eigs(Sz,9);
 A2=A(:,1:2);
 A2r=rotatefactors(A2);
+
 %3. Compute K-means on the number of component identified in the step 1;
 Y=Z*A2r;
 [vc2,Ym2]=kmeans(Y,2,'Replicates',100);
 [vc3,Ym3]=kmeans(Y,3,'Replicates',100);
+
 %4. Identify the best K with pF;
 I2=eye(2)
 U2=I2(vc2,:);
@@ -83,6 +94,7 @@ title('Tandem Analysis');
 legend('Cluster 1', 'Cluster 2', 'Centroids');
 grid on;
 hold off;
+
 %5. Compute Reduced K-mean;
 [Urkm,Arkm, Yrkm,frkm,inrkm]=REDKM(Z, 2, 2, 20);
 pf2=psF(Yrkm,Urkm)
@@ -97,6 +109,7 @@ title('Reduced K-Means Clustering');
 legend('Cluster 1', 'Cluster 2');
 grid on;
 hold off;
+
 %6. Compute Factorial K-means;
 [Ufkm,Afkm, Yfkm,ffkm,infkm]=FKM(Z, 2, 2, 20);
 U2'*Ufkm
@@ -120,12 +133,14 @@ title('Classification of Objects on First Two Dimensions of Factorial K-means');
 legend show;
 grid on;
 hold off;
+
 %7. Compute Clustering and Disjoint PCA;
 [Vcdpca,Ucdpca,Acdpca, Ycdpca,fcdpca,incdpca]=CDPCA(Z, 2, 2, 20);
 [Vcdpca3,Ucdpca3,Acdpca3, Ycdpca3,fcdpca3,incdpca3]=CDPCA(Z, 2, 3, 20);
 pf2CDP=psF(Ycdpca,Ucdpca)
 pf3CDP=psF(Ycdpca3,Ucdpa3)
 [Vdpca,Adpca, Ydpca, fdpca, indpca] = DPCA(Z,2,20)
+
 % Create a heatmap of the loadings matrix
 figure;
 h = heatmap(Acdpca);
@@ -152,9 +167,11 @@ legend('Cluster 1', 'Cluster 2');
 grid on;
 axis equal;
 hold off;
+
 %8. Compute the Double K-means.
 [Vdkm,Udkm,Ymdkm, fdkm,indkm]=DKM(Z, 2, 2, 20);
 [Vdkm3,Udkm3,Ymdkm3, fdkm3,indkm3]=DKM(Z, 2, 3, 20);
+
 %9. Compare the results by using the confusion matrix (contingency table);
 %confusion matrix between 4 and 5
 U2'*Urkm
